707060f05206d2fe09b90f8e9a2c289ac5b6d66c
cslite <34658024+cslite@users.noreply.github.com>
2019-02-04 22:01:29 +0530

    Update list.h

--- list.h
+++ list.h
@@ -1,13 +1,13 @@
 
-
-void set_ram(int size);		//allocate ram (array) of given size
+int get_next_lno();		//to get the number of next new list
+void allocate_ram(int size);//allocate ram (array) of given size and perform some initializations	
 void push(int node);		//to push an object to the free_list list
 int pop();					//to pop an object from the free_list list
-int newlist(int key);		//create a new list with key as the first node's value and return the head
-int insert(int head, int key);	//insert the key in sorted order in list starting at head
-int delete(int head, int key);	//delete the first occurrence of key in the list starting at head
-int count_nodes(int head);	//to count number of nodes of the given list
+int createlist(int key);		//create a new list with key as the first node's value at next new list position
+int insert(int lno, int key);	//insert the key in sorted order in list at list no. lno
+int delete(int lno, int key);	//delete the first occurrence of key in the list at list no. lno
+int count_nodes_lno(int lno);	//to count number of nodes of the given list
 int count_total();			//count total non-free nodes in the memory
-void display(int head);		//to display all nodes of the given list
+void displayAll();		//to display all nodes of all lists
 void displayFree();			//to display nodes of the free_list
-int defrag(int *list_seq,int n);	//to perform defragmentation
+int defrag();	//to perform defragmentation
